3f41a3198a1f218aa0d747f6ad7676ca3dad53a5
jin-raii <jinrai577@gmail.com>
2026-01-12 21:18:25 +0545

    fix ollama error

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os 
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task, LLM
+import asyncio
 
-
-def main():
-    print("Hello from summurize-blog!")
-
+async def main():
+    llm = LLM(model="ollama/olmo-3:7b", temperature=0.7, max_tokens=512, streaming=True, base_url="http://localhost:11434")
+    response = await llm.acall('what is the capital of Nepal?')
+    print(response)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
