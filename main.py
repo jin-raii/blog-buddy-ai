b9ff2f361462cdbbebfdf6e955a0a9b7a610c1e0
jin-raii <jinrai577@gmail.com>
2026-01-12 21:37:38 +0545

    add langchain ollama

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os 
 from crewai import Agent, Crew, Process, Task, LLM
 import asyncio
+from langchain_ollama import ChatOllama
 
 async def main():
-    llm = LLM(model="ollama/olmo-3:7b", temperature=0.7, max_tokens=512, streaming=True, base_url="http://localhost:11434")
-    response = await llm.acall('what is the capital of Nepal?')
-    print(response)
+    # llm = LLM(model="ollama/olmo-3:7b", temperature=0.7, max_tokens=512, base_url="http://localhost:11434")
+    llm = ChatOllama(model="olmo-3:7b", base_url="http://localhost:11434", temperature=0.7, max_tokens=512)
+    # response = await llm.acall('what is the capital of Nepal?')
+    response = llm.invoke('what is the capital of Nepal?')
+    print(response.text)
 
 if __name__ == "__main__":
     asyncio.run(main())
