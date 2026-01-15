41d4d40f9e776fa52a1414d41dfe8b3069fa515a
jin-raii <jinrai577@gmail.com>
2026-01-15 20:41:26 +0545

    add task and crew instanace

--- main.py
+++ main.py
@@ -7,61 +7,61 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-async def main():
-    # llm = LLM(model="ollama/olmo-3:7b", temperature=0.7, max_tokens=512, base_url="http://localhost:11434")
-    llm = ChatOllama(model="olmo-3:7b", base_url="http://localhost:11434", temperature=0.7, max_tokens=512)
-    # response = await llm.acall('what is the capital of Nepal?')
-    # prompt = "what is the capital of Nepal?"
-    # # print(response.text)
-    # message = [
-    #     ('system', 'You are a helpful assistant.'),
-    #     ('user', prompt)
-    # ]
-    # response = llm.invoke(message)
-    # stream = llm.stream(response.content)
-    # full = next(stream)
-    # for chunk in stream:
-    #     full += chunk
-    # print(full, end='', flush=True)
-   
-    tools = [
-        FirecrawlScrapeWebsiteTool(
-            api_key=os.getenv("FIRECRAWL_ENV"),
-        )
-    ]
-    
 
-    # create an agent with the LLM and tools
-    scrape_agent = Agent(
-        name="ScrapeAgent",
-        role="A web scraping agent that uses Firecrawl to scrape websites.",
-        goal="Extract relevant information from websites using Firecrawl.",
-        backstory="You are an expert web scraper with access to Firecrawl.",
-        llm=llm,
-        tools=tools,
-        Verbose=True, 
-        allow_delegation=False
+# llm = LLM(model="ollama/olmo-3:7b", temperature=0.7, max_tokens=512, base_url="http://localhost:11434")
+llm = ChatOllama(model="olmo-3:7b", base_url="http://localhost:11434", temperature=0.7, max_tokens=512)
+# response = await llm.acall('what is the capital of Nepal?')
+# prompt = "what is the capital of Nepal?"
+# # print(response.text)
+# message = [
+#     ('system', 'You are a helpful assistant.'),
+#     ('user', prompt)
+# ]
+# response = llm.invoke(message)
+# stream = llm.stream(response.content)
+# full = next(stream)
+# for chunk in stream:
+#     full += chunk
+# print(full, end='', flush=True)
 
+tools = [
+    FirecrawlScrapeWebsiteTool(
+        api_key=os.getenv("FIRECRAWL_ENV"),
     )
+]
+
+
+# create an agent with the LLM and tools
+scrape_agent = Agent(
+    name="ScrapeAgent",
+    role="A web scraping agent that uses Firecrawl to scrape websites.",
+    goal="Extract relevant information from websites using Firecrawl.",
+    backstory="You are an expert web scraper with access to Firecrawl.",
+    llm=llm,
+    tools=tools,
+    Verbose=True, 
+    allow_delegation=False
+
+)
 
 
 
-    agent_summarizer = Agent(
-        name="Summarizer Agent",
-        role="A summarization agent that summarizes text content.",
-        goal="Summarize the extracted content into concise summaries.",
-        backstory="You are an expert summarizer.",
-        llm=llm,
-        verbose=True,
-        allow_delegation=False
-    )
+agent_summarizer = Agent(
+    name="Summarizer Agent",
+    role="A summarization agent that summarizes text content.",
+    goal="Summarize the extracted content into concise summaries.",
+    backstory="You are an expert summarizer.",
+    llm=llm,
+    verbose=True,
+    allow_delegation=False
+)
 
-    # define task 
+# define task 
 
 def scrape_task(url: str, agent: Agent):
     return Task(
         description=(f"Scrape the website at {url} using FirecrawlScrapeWebsiteTool."
-                     "Extract the main content, headings, and any relevant metadata. filtering out ads and navigation elements."),
+                    "Extract the main content, headings, and any relevant metadata. filtering out ads and navigation elements."),
         expected_output="Extracted content from the website.",
         agent=agent,
     ) 
@@ -69,12 +69,32 @@ def scrape_task(url: str, agent: Agent):
 def summarize_task(content: str, agent: Agent):
     return Task(
         description=(f"Summarize the extracted content.{content}."
-                     "Provide a concise summary highlighting the key points and main ideas."),
+                    "Provide a concise summary highlighting the key points and main ideas."),
         expected_output=("A concise summary of the content."
-                         "Ensure the summary is clear and captures the essence of the original text."),
+                        "Ensure the summary is clear and captures the essence of the original text."),
         agent=agent,
+        context=[content]
     )
+
+# create a crew with the agents
+def crew_process(url: str, scrape_agent: Agent, summarizer_agent: Agent):
+    scrape_task = scrape_task(url, scrape_agent)
+    summarize_task = summarize_task(scrape_task, summarizer_agent)
+
+    crew = Crew(
+        agents=[scrape_agent, summarizer_agent],
+        tasks=[scrape_task, summarize_task],
+        verbose=True,
+        process=Process.SEQUENTIAL
+    )
+
+    return crew 
+
+
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    url = 'https://ekantipur.com/'
+    crew = crew_process(url, scrape_agent, agent_summarizer)
+     # run the crew
+    asyncio.run(crew.kickoff())
