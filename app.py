25c106992c534fc038945b63198cbcea2532ef65
jin-raii <jinrai577@gmail.com>
2026-01-17 21:10:59 +0545

    add generate audio button and output audio

--- app.py
+++ app.py
@@ -3,19 +3,29 @@ from pocket_tts import TTSModel
 import scipy.io.wavfile 
 
 def generate_audio(text):
-    tts_model = TTSModel.load_model()
-    voice_state = tts_model.get_state_for_audio_prompt("hf://kyutai/tts-voices/alba-mackenna/casual.wav")
-
-    audio = tts_model.generate_audio(voice_state, "hello world, how are you?")
-    scipy.io.wavfile.write("output.wav", tts_model.sample_rate, audio.numpy())
-
+    try:
+        # while generating audio, show spinner on UI
+        progress = gr.Progress()
+        progress(0.5, "Generating audio...")
+        print('Generating audio for the text...')
+        tts_model = TTSModel.load_model()
+        progress(0.4, "Model loaded.")
+        voice_state = tts_model.get_state_for_audio_prompt("hf://kyutai/tts-voices/alba-mackenna/casual.wav")
+        progress(0.7, "Voice state prepared.")
+        audio = tts_model.generate_audio(voice_state, text)
+        progress(1.0, "Audio generation complete.")
+        scipy.io.wavfile.write("output.wav", tts_model.sample_rate, audio.numpy())
+        print("Audio generated and saved as output.wav")
+    except Exception as e:
+        print("Error generating audio:", e)
 
 def UI():
     with gr.Blocks() as demo:
         gr.Markdown("# Blog Summarizer")
-        url_input = gr.Textbox(label="Enter Blog URLs (comma separated)", placeholder="https://example.com/blog1, https://example.com/blog2")
+        url_input = gr.Textbox(label="Enter Blog URLs (comma separated)", placeholder="https://example.com")
         summarize_button = gr.Button("Summarize")
         output_area = gr.Textbox(label="Summary Output", lines=20)
+        audio_button = gr.Button("Generate Audio")
 
         def summarize_blogs(urls):
             url_list = [url.strip() for url in urls.split(',')]
@@ -24,7 +34,17 @@ def UI():
             summary = summarize_content(url_list)
             return summary
 
+        def generate_audio_from_summary(summary):
+            print("Generating audio for the summary...", summary, end='')
+            generate_audio(summary)
+
         summarize_button.click(fn=summarize_blogs, inputs=url_input, outputs=output_area)
+        audio_button.click(fn=generate_audio_from_summary, inputs=output_area, outputs=None)
+
+        # after generating audio, show audio on the UI 
+        audio_output = gr.Audio(label="Generated Audio", streaming=True)
+        audio_button.click(fn=lambda: "output.wav", inputs=None, outputs=audio_output)
+
 
     demo.launch()
 
